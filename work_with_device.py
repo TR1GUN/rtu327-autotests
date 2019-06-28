a4ea94da50846db257d566eeba370ccbb638a72e
Купырев Александр Валерьевич <a.kupyrev@allmonitoring.ru>
2019-06-28 15:24:07 +0300

    +

--- work_with_device.py
+++ work_with_device.py
@@ -65,24 +65,29 @@ TableCRC = [0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7, 0x81
             0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0, 0x0cc1, 0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9,
             0x9ff8, 0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1, 0x1ef0]
 
+def char_bytes_str_to_array(request):
+    # print(parse_bytes_str_to_array(request))
+    return hex_bytes_array_to_string([bytes.hex(_) for _ in parse_bytes_str_to_array(request)])
 
-def parse_bytes_str_to_array(request):
-    """ Кидать b'' команды . НЕ STR """
+def parse_bytes_str_to_array(request,add_x_prefix=True):
+    """ Кидать b'' команды . НЕ STR
+    ??? Просто разбитые, не в Char представлении ???
+    """
     result_array = []
     for _ in str(request).split(r'\x')[1:]:  ## Не учтен вариант если спереди уже байтовое представление
         # Первый и последний убираем, т.к. они не нужны. -- "b'" и b"'" соответственно.
-        #     print(str(request).split(r'\x'))
-        #     print(_)
         if _[0] == 'x' and len(_) > 2: _ = _[1:]  ## Проверяем x00
         if len(_) == 2:
-            result_array.append(_)
+            if add_x_prefix : result_array.append(bytes.fromhex(_))
+            else : result_array.append(_)
         elif len(_) <= 1:
             raise Exception("Can't be 1 symbol")
         else:
-            first_byte = _[:2]
+            if add_x_prefix : first_byte =bytes.fromhex(_[:2])
+            else : first_byte = _[:2]
             other_bytes = [_[2:]]
             result_array.append(first_byte)
-            temp_arr = [x for x in list(' '.join(other_bytes))]
+            temp_arr = [x.encode() for x in list(' '.join(other_bytes))]
             for _ in temp_arr: result_array.append(_)
     return result_array[:-1]  ## последний ' остался от b''
 
@@ -245,7 +250,7 @@ def send_command_and_get_answer(command_number=None, command_params=b'', send_co
     res.close()
 
     hex_normal_view_answer_array = hex_bytes_array_to_string(answer_bytes)
-
+    print(hex_normal_view_answer_array,'parsed_answer')
     """ Проверить crc """
     if answer_bytes[-3:] == ['3c', 'a0', '2f']: print('bad answer')
     return parse_answer(hex_normal_view_answer_array)
@@ -253,3 +258,4 @@ def send_command_and_get_answer(command_number=None, command_params=b'', send_co
 
 # print(get_crc(b'\x01\x00\x00\x00\x00\x00\x00\x00s\x00\x00\x00\x39'))
 # print(parse_bytes_str_to_array(b'\x01\x00\x00\x00\x00\x00\x00\x00s9\x00\x00\x00'))
+# print(char_bytes_str_to_array(b'\x01\x00\x00\x00\x00\x00\x00\x00s9\x00\x00\x00'))
